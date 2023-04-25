0862e44bb9668d8797ca162e67607e71a8b3b779
RafaelClaudio7 <rc7true@gmail.com>
2023-04-25 19:43:59 -0300

    modifying try catch to promises

--- src/controllers/PostsController.ts
+++ src/controllers/PostsController.ts
@@ -5,12 +5,14 @@ const postsServices = new PostsServices();
 
 export class PostsController {
   async listAll(req: Request, res: Response) {
-    try {
-      const allPosts = await postsServices.findAllPosts();
-      res.status(200).json(allPosts);
-    } catch (error) {
-      res.status(500).send(error);
-    }
+    postsServices
+      .findAllPosts()
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
   }
 
   async list(req: Request, res: Response) {
